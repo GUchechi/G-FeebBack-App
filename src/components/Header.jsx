8f973118cd763c8e643c00d537bbbb0ddb2c7dbf
GUchechi <godswilluchendu7@gmail.com>
2023-01-12 13:28:16 +0100

    feedback lists and items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Header = ({ text }) => {
+const Header = ({ text, textColor, bgColor }) => {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
@@ -13,10 +18,14 @@ const Header = ({ text }) => {
 
 Header.defaultProps = {
   text: "FeedBack UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
 };
 
 Header.propTypes = {
   text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 };
 
 export default Header;
