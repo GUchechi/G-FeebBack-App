9657adfacccb66dbd90575545a3bd6f2f10c1736
GUchechi <godswilluchendu7@gmail.com>
2023-01-13 02:13:59 +0100

    form validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,22 @@ import Card from "./shared/Card";
 
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
+    e.preventDefault();
     setText(e.target.value);
   };
 
@@ -22,8 +36,11 @@ const FeedbackForm = () => {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
